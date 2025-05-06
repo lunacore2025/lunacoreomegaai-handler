710ca212205b1ad343f0ad63f62802d4c8af52ec
lunacore2025 <sargas1223@gmail.com>
2025-05-06 14:47:27 +0900

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-{
-  "name": "lunacoreomegaai-handler",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "engines": {
-    "node": "18.x"
-  },
-  "dependencies": {
-    "express": "^4.18.2"
-  }
-}
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from lunacoreomegaAI-handler!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
